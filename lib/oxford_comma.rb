486ee11140a529b4b783952e7ed188b826671f97
Jashema Panapa <jcpanapa@gmail.com>
2020-03-29 00:22:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
+
+## I had to go back and do this one completely over. The first two tasks, I got. But the way that I did it, I couldn't complete the last two without ".map". Then went to SoloLearn to review indices in arrays
