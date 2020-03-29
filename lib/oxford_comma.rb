b1c7cb31e2753c3d7a6b1f8e09bab63f129e78af
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-03-29 00:15:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else array.length >= 3
+    new_array = array.pop()
+      "#{array.join(", ")}, and #{new_array}"
+  end
 end
