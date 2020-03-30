919d3fde5b41f4401ff3b944d02df2fc0c78ac91
Tanner Townsend <rsmcwow@gmail.com>
2020-03-30 00:51:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    message = array.join
+    return message
+  elsif array.length == 2
+    message = array[0...-1].join
+    message += " and #{array[-1]}"
+    return message
+  else
+    message = array[0...-1].join(", ")
+    message += ", and #{array[-1]}"
+    return message
+  end
 end
