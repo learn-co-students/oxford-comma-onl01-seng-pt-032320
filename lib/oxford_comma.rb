7a5755e7af374aaf5dd6d5488359614af26530b7
Dina Kendle <dina.kendle@gmail.com>
2020-04-01 12:26:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
+#def oxford_comma(array)
+ # array.join(", ") 
+#end
+
 def oxford_comma(array)
-   array.join(", ") + array[-1].join(" and ")
-    
+  if array.length == 1
+    array.join()
+  elsif array.length > 1 && array.length < 3
+    array.join(" and ")
+  else 
+    array[0...-1].join(", ") +"," + " and " + array[-1]
+  end
 end
