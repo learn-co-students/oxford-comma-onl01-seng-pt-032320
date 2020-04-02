4d1786662f391bf8d30318331ef88b57907f01d8
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-02 04:43:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    return array.join(" and ")
+  end
+  if array.size >= 3
+    array[array.size-1] = "and #{array[array.size-1]}"
+    return array.join(", ")
+  end
+  array.join
 end
