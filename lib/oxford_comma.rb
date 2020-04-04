b0f5ea4d323344d9dd7f5d3ea9f04fe5ff052b18
Joshua Williams <j.o-williams@outlook.com>
2020-04-04 16:04:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    # convert to string and add an "and"
+    array.join(" and ")
+  else 
+  	#if we get here, we know there are more than two items
+    #add commas and a final "and"
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
 end
